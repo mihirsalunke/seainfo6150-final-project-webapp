59ff61b12ef8a3072cfe25d64a4d1d9dfd845349
Mihir Salunke <salunke.mi@husky.neu.edu>
2020-12-12 01:01:34 -0500

    Updates

---
+++ precache-manifest.00ac8022e1bf54949bf9a27e2645e382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e427509f9da5d746dbe40bcf72b56dd0",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "fa6abdbfc9ee47750f3d",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.b3049aed.chunk.css"
+  },
+  {
+    "revision": "6d7aad83b91488db2d64",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.ba697726.chunk.js"
+  },
+  {
+    "revision": "fa6abdbfc9ee47750f3d",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.99838a8b.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "648a42ff07392712adffc28024c44dcc",
+    "url": "/seainfo6150-final-project-webapp/static/media/loading.648a42ff.gif"
+  }
+]);
